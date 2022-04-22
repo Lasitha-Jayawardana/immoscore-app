0ede55a540d987d887d174e97af5e3b63755c28b
Lasitha <lsjayawardana95@gmail.com>
2022-04-23 01:00:36 +0530

    fix

--- src/utils/constants/SidebarData.tsx
+++ src/utils/constants/SidebarData.tsx
@@ -32,10 +32,6 @@ export const SidebarMenuData:SidebarMenu[] = [
         icon: <FaIcons.FaGripfire/>,
         title: 'Landlords',
         path: "#Landlords",
-    }, {
-        icon: <FaIcons.FaMagento/>,
-        title: 'Ponols',
-        path: "#Ponols",
     },
     {
         title: "Pools",
