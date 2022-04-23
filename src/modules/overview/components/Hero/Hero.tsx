e6035aec6f07148352eb5df09b7eeb03ad6e7816
Lasitha <lsjayawardana95@gmail.com>
2022-04-23 20:04:56 +0530

    chart added

--- src/modules/overview/components/Hero/Hero.tsx
+++ src/modules/overview/components/Hero/Hero.tsx
@@ -1,6 +1,7 @@
 import {FC} from "react";
 import './Hero.scss'
 import {LabelCard} from "../../../../shared/components/core-components/LabelCard/LabelCard";
+import {Currency} from "../Chart";
 
 export const Hero: FC = (props) => {
     return (
@@ -9,10 +10,10 @@ export const Hero: FC = (props) => {
                 <LabelCard color={"#4f8fea"} title={"Total Value Locked (USD)"} value={"$211.21b"}/>
                 <LabelCard color={"#fc3c98"} title={"Change (24h)"} value={"3.61%"}/>
                 <LabelCard color={"#46abb6"} title={"Curve Dominance"} value={"9.66%"}/>
-
             </div>
             <div className={"hero-right-container"}>
-
+                <Currency/>
+                {/*<Chart/>*/}
             </div>
         </div>
     )
