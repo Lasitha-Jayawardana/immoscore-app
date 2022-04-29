f6740bf5cdf06a9d775e35afd5d7b14014d8e65e
Lasitha <lsjayawardana95@gmail.com>
2022-04-29 14:27:48 +0530

    typeform integrated

--- src/shared/components/core-components/ConnectWallet/ConnectWallet.tsx
+++ src/shared/components/core-components/ConnectWallet/ConnectWallet.tsx
@@ -1,4 +1,4 @@
-import {FC, ReactNode} from "react";
+import React, {FC, ReactNode, useState} from "react";
 import './ConnectWallet.scss'
 import {FaRegBookmark} from "react-icons/fa";
 import {CircleIcon} from "../CircleIcon/CircleIcon";
@@ -6,35 +6,41 @@ import Icon from "../../../../assests/img/immoscore.png"
 
 interface Props {
     onClose?: ()=>void;
+    setTypeFrom: (e:boolean)=>void;
 }
 
 export const ConnectWallet: FC<Props> = (props) => {
     return (
-        <div className={"wallet"}>
-            <button onClick={props.onClose} className={"wallet-close-btn"}>
+        <>
+           <div className={"wallet"}>
+                <button onClick={props.onClose} className={"wallet-close-btn"}>
                 <span>
                     <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg"><path
                         d="M13 1 1 13M1 1l12 12" stroke="#BDBDBD" stroke-width="2" stroke-linecap="round"
                         stroke-linejoin="round"></path></svg>
                 </span>
-            </button>
-            <div className={"wallet-container"}>
-                <div className={"wallet-top-container"}>
-                    {/*<CircleIcon icon={Icon} size={"72px"} bColor={"transparent"}/>*/}
-                    {/*<div style={{height:"72px",width:"72px", borderRadius:"10px"}}>*/}
+                </button>
+                <div className={"wallet-container"}>
+                     <>
+                    <div className={"wallet-top-container"}>
+                        {/*<CircleIcon icon={Icon} size={"72px"} bColor={"transparent"}/>*/}
+                        {/*<div style={{height:"72px",width:"72px", borderRadius:"10px"}}>*/}
                         <img style={{height:"72px" }} src={Icon}/>
-                    {/*</div>*/}
-                    <h3>Want Early Access?</h3>
-                    <p>We are allowing a <b>selected</b> group of users to get the first access.</p>
-                </div>
-                <hr/>
-                <button className={"wallet-btn"}>Request Early Access</button>
-                <p>Immoscore is coming soon.</p>
-                <div>
+                        {/*</div>*/}
+                        <h3>Want Early Access?</h3>
+                        <p>We are allowing a <b>selected</b> group of users to get the first access.</p>
+                    </div>
+                    <hr/>
+                    <button onClick={()=>props.setTypeFrom(true)} className={"wallet-btn"}>Request Early Access</button>
+                    <p>Immoscore is coming soon.</p>
+                </>
 
                 </div>
             </div>
-        </div>
+
+
+                </>
+
     )
 
 
--- src/shared/components/layout/Modal/Modal.tsx
+++ src/shared/components/layout/Modal/Modal.tsx
@@ -2,6 +2,7 @@ import React, {FC, ReactNode, useState} from "react";
 import ReactModal from 'react-modal';
 import {ConnectWallet} from "../../core-components/ConnectWallet/ConnectWallet";
 import "./Modal.scss"
+import {Widget} from "@typeform/embed-react";
 
 const customStyles = {
     content: {
@@ -10,12 +11,12 @@ const customStyles = {
         right: 'auto',
         bottom: 'auto',
         marginRight: '-50%',
-        backgroundColor: "#4a4a4a",
+        // backgroundColor: "#4a4a4a",
+        background:'transparent',
         transform: 'translate(-50%, -50%)',
-        border: "1px solid rgb(45, 16, 212)",
-        borderRadius: "24px",
-        boxShadow: "0 3px 6px -4px rgba(0, 0, 0, 0.12), 0 6px 16px 0 rgba(0, 0, 0, 0.08), 0 9px 28px 8px rgba(0, 0, 0, 0.05)",
-    }
+        // border: "1px solid rgb(45, 16, 212)",
+        border: "none",
+        }
 };
 
 interface Props {
@@ -26,6 +27,7 @@ interface Props {
 export const Modal: FC<Props> = (props) => {
 
     const [modalIsOpen, setIsOpen] = React.useState(props.isOpen);
+    const [typeFrom,setTypeFrom]=useState(false)
 
     function openModal() {
         setIsOpen(true);
@@ -50,7 +52,18 @@ export const Modal: FC<Props> = (props) => {
                 style={customStyles}
                 contentLabel="Connect Wallet"
                 overlayClassName={"modal-overlay"}>
-                <ConnectWallet onClose={closeModal}/>
+                {!typeFrom ?
+                    <ConnectWallet setTypeFrom={setTypeFrom} onClose={closeModal}/>
+                    :   <div style={{width:"60vw",height:"80vh",background:"transparent"}}>
+                        <button onClick={closeModal} className={"modal-close-btn"}>
+                <span>
+                    <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg"><path
+                        d="M13 1 1 13M1 1l12 12" stroke="#BDBDBD" stroke-width="2" stroke-linecap="round"
+                        stroke-linejoin="round"></path></svg>
+                </span>
+                        </button>
+                        <Widget id="frbzhUjk" style={{width:"100%",height:"100%", border: "1px solid rgb(45, 16, 212)"}}/>
+                    </div>}
                 {props.children}
             </ReactModal>
         </div>
